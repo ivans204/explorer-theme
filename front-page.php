8b93beda97a531b1baca0d708ced1d21abd99894
ivans204 <ivansvrga204@gmail.com>
2021-02-12 00:45:27 +0100

    about us on front page

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header('front'); ?>
 <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
-		<section id="explore">
+		<section id="explore" class="clouds-bg">
 
 			<div class="container">
 				<div class="row">
@@ -74,7 +74,7 @@
 
 		</section>
 
-		<section id="explore-area">
+		<section id="explore-area" class="clouds-bg">
 
 			<div class="container">
 				<div class="row">
@@ -109,10 +109,9 @@
 
 		</section>
 
+		<!--  Section Quotes  -->
 		<section id="explore-quotes">
 			<div class="slideshow-container">
-
-				<h2 class="reviews-title">Reviews</h2>
 
                 <?php
                 $args = [
@@ -120,6 +119,13 @@
                     'posts_per_page' => -1
                 ];
                 $query = new WP_Query($args);
+
+                if (!empty($query->posts))
+                {
+                ?>
+				<h2 class="reviews-title">Reviews</h2>
+
+                <?php
                 foreach ($query->posts as $key => $post)
                 {
                 if ($key === 0)
@@ -142,11 +148,41 @@
 					<!-- Next/prev buttons -->
 					<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 					<a class="next" onclick="plusSlides(1)">&#10095;</a>
+                    <?php } ?>
 				</div>
 		</section>
-		<section id="about-us"></section>
+
+		<section id="about-us" class="clouds-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-12 col-sm-12">
+                        <?php
+                        $page = get_page_by_title('O nama');
+                        ?>
+						<h1 class="mb-4"><?= get_the_title($page) ?></h1>
+						<p class="about-us-text"><?= wp_trim_words($page->post_content, 100) ?></p>
+						<a class="about-us-link" href="<?= get_permalink($page) ?>">Saznaj više <i
+									class="icon icon-green-arrow"></i></a>
+					</div>
+					<div class="col-lg-4 col-md-6 col-sm-12">
+						<img class="about-us-img" src="<?= get_the_post_thumbnail_url($page) ?>" alt="">
+					</div>
+					<div class="col-lg-2 col-md-6 col-sm-12">
+                        <?php
+                        if (class_exists('Dynamic_Featured_Image'))
+                        {
+                            global $dynamic_featured_image;
+                            $featured_image = $dynamic_featured_image->get_featured_images($page->ID)[0]['full'];
+
+                            echo "<img class='about-us-img' src='$featured_image'>";
+                        }
+                        ?>
+					</div>
+				</div>
+			</div>
+		</section>
 		<section id="newsletter">
-            <?php echo do_shortcode('[newsletter_form]') ?>
+			<!--            --><?php //echo do_shortcode('[newsletter_form]') ?>
 		</section>
 
     <?php endwhile; ?>
