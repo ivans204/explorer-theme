d7c0be8c3a9b12962f75b7c7c0a41c224ead4644
ivans204 <ivansvrga204@gmail.com>
2021-02-17 15:01:22 +0100

    front page explore sea and sky

--- template-parts/front_page/explore-area.php
+++ template-parts/front_page/explore-area.php
@@ -1,35 +1,47 @@
+<?php
+
+global $product;
+
+$sky = get_page_by_title('Explore the sky', OBJECT, 'product');
+$sea = get_page_by_title('Explore the sea', OBJECT, 'product');
+$sky_prod = wc_get_product($sky->ID);
+$sea_prod = wc_get_product($sea->ID);
+
+//var_dump($sea->get_image());
+?>
+
 <section id="explore-area" class="clouds-bg">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
 
-                <div class="explore-area">
-                    <img class="explore-area_img"
-                         src="<?= get_template_directory_uri() . '/assets/img/A-2.jpg' ?>" alt="">
-                    <h2 class="explore-area_title">Explore the sky</h2>
-                    <a class="explore-area_more" href="#">Saznaj više -> </a>
-                </div>
+				<div class="explore-area">
+					<img class="explore-area_img"
+					     src="<?= wp_get_attachment_url( $sky_prod->get_image_id() ); ?>" alt="">
+					<h2 class="explore-area_title"><?= __('Explore the sky') ?></h2>
+					<a class="explore-area_more d-flex justify-content-center align-items-center" href="<?= $sky_prod->get_permalink(); ?>">Saznaj više <i class="icon icon-arrow"></i> </a>
+				</div>
 
 
-            </div>
-            <div class="col-lg-6">
+			</div>
+			<div class="col-lg-6">
 
-                <div class="explore-area">
-                    <img class="explore-area_img"
-                         src="<?= get_template_directory_uri() . '/assets/img/IMG_-2.jpg' ?>" alt="">
-                    <h2 class="explore-area_title">Explore the sea</h2>
-                    <a class="explore-area_more" href="#">Saznaj više -> </a>
-                </div>
-            </div>
-        </div>
-    </div>
+				<div class="explore-area">
+					<img class="explore-area_img"
+					     src="<?= get_template_directory_uri() . '/assets/img/IMG_-2.jpg' ?>" alt="">
+					<h2 class="explore-area_title"><?= __('Explore the sea') ?></h2>
+					<a class="explore-area_more d-flex justify-content-center align-items-center" href="<?= $sea_prod->get_permalink(); ?>">Saznaj više <i class="icon icon-arrow"></i> </a>
+				</div>
+			</div>
+		</div>
+	</div>
 
-    <div class="section-end beach-img"></div>
+	<div class="section-end beach-img"></div>
 
     <?php echo do_shortcode('[metaslider id="57"]'); ?>
-<!--    --><?php //echo do_shortcode('[metaslider id="159"]'); ?>
+	<!--    --><?php //echo do_shortcode('[metaslider id="159"]'); ?>
 
-    <p class="slider-desc">Slike s naših tura</p>
+	<p class="slider-desc">Slike s naših tura</p>
 
 </section>
