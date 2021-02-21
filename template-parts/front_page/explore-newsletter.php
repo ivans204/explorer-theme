fb07e3c051a9cb5a4a24ebc59d30bd14a0e7b303
ivans204 <ivansvrga204@gmail.com>
2021-02-21 21:55:44 +0100

    newsletter form

--- template-parts/front_page/explore-newsletter.php
+++ template-parts/front_page/explore-newsletter.php
@@ -1,4 +1,14 @@
 <section id="newsletter" class="clouds-bg" style="margin-top: 100px">
-    <!--            --><?php //echo do_shortcode('[newsletter_form]') ?>
-    <?= get_custom_logo() ?>
+
+	<div class="container">
+		<div class="newsletter-box">
+			<h1>Newsletter</h1>
+			<p><?= get_theme_mod('newsletter-text') ?></p>
+            <?php echo do_shortcode('[newsletter_form type=“minimal”][newsletter_field name="email"][/newsletter_form]') ?>
+		</div>
+
+        <?= get_custom_logo() ?>
+	</div>
+
+
 </section>
